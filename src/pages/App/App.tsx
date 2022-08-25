39c7503cb476db82a7fcf57ee19f890c928afcf0
willmister <william.nordqvist@tink.com>
2022-08-25 11:43:01 +0200

    add searchbar

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,12 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import {
+  recruiterSteps,
+  useCandidates,
+} from "../../context/candidateContext";
+import { Input, Select, ClearInput, InputWrapper } from "./AppStyle";
 
 export const App: React.FC = () => {
+  const candidates = useCandidates();
+  const [searchWord, setSearchWord] = useState("");
+  const [selectedStage, setSelectedStage] = useState("");
+
+  if(!candidates){
+    return <p>Loading</p>
+  }
+
   return (
     <div>
-      <Link to="/add">
+      <InputWrapper>
+        <Input
+          id="search"
+          name="search"
+          placeholder="SEARCH"
+          value={searchWord}
+          onChange={(e) => setSearchWord(e.target.value)}
+        />
+
+        <Select
+          onChange={(e) => setSelectedStage(e.target.value)}
+          value={selectedStage}
+          name="Stage"
+          id="Stage"
+        >
+          <option hidden selected>
+            FILTER
+          </option>
+          {recruiterSteps.map((step) => (
+            <option value={step}>{step}</option>
+          ))}
+        </Select>
+        <ClearInput fontSize="small" onClick={() => setSelectedStage(" ")}/>
+      </InputWrapper>
+      {/* <Link to="/add">
         <h1>APP</h1>
-      </Link>
+      </Link> */}
     </div>
   );
 };
---
+++ src/pages/App/AppStyle.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import ClearIcon from '@mui/icons-material/Clear';
+
+export const InputWrapper = styled.div`
+display:flex;
+padding-top:6%;
+margin:auto;
+width:90%;
+max-width:700px ;
+border-bottom: 2px solid ${({ theme }) => theme.primary};
+`
+
+export const Input = styled.input`
+  width: 100%;
+  background: transparent;
+  border: none;
+  padding: 0;
+  color: ${({ theme }) => theme.primary};
+  border-radius: 0px;
+  font-size: 16px;
+
+  ::placeholder {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+export const Select = styled.select`
+  width: 30%;
+  background: transparent;
+  border: none;
+  font-family: Raleway;
+  color: ${({ theme }) => theme.primary};
+  text-transform:uppercase;
+  font-size:16px ;
+`;
+
+export const Label = styled.label`
+  color: ${({ theme }) => theme.primary};  
+`;
+
+export const ClearInput = styled(ClearIcon)`
+border:none;
+background:transparent;
+color: ${({ theme }) => theme.primary};
+margin-left:5px;
+`
